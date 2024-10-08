e13c343b76255f74517b192093f1d3cc1fa01fa4
ZayyTheWizard <isaiah.washington19@yahoo.com>
2024-10-07 21:20:50 -0400

    pasrse and parse tree

--- source/core/main.py
+++ source/core/main.py
@@ -1,15 +1,17 @@
-from tokenizer import CodeTokenizer
+import ast
+from ast_parser import ast_parser
 
 
 if __name__ == "__main__":
-    
-
     file_path = 'source/tests/test.py'
     
+    astTree = ast_parser(file_path)
+    print(ast.dump(astTree, indent=4))
     # Initialize tokenizer with the file path
-    tokenizer = CodeTokenizer(file_path)
-    
-    # Tokenize the code
-    tokenizer.tokenize_code()
-
-    print(tokenizer.get_tokens())
+    #tokenizer = CodeTokenizer(file_path)
+    #
+    ## Tokenize the code
+    #tokenizer.tokenize_code()
+    #ast = syntaxTree(tokenizer.get_tokens())
+    #myTree = ast.build_tree()
+    #myTree.displayBFS()

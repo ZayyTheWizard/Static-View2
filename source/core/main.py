fa67b9cffd654f568f1401453a0cfd7222daba5f
CJ Obizuo <66581590+cobizuo@users.noreply.github.com>
2024-10-16 09:40:41 -0400

    AST Parser and Test code cases
    Allowed for ast_parser.py input from a file and reading to a file for logging and reporting features in the future
    
    Generated functions to identify functions within the ast to prepare for vulnerablility pattern matching
    
    Generated test code for progression monitoring

--- source/core/main.py
+++ source/core/main.py
@@ -6,13 +6,10 @@ if __name__ == "__main__":
     file_path = 'source/tests/test.py'
     
     astTree = ast_parser(file_path)
-    if astTree is not None:
-        print(ast.dump(astTree, indent=4))
-    else:
-        pass
+    # if astTree is not None:
+    #     print(ast.dump(astTree, indent=4))
     # Initialize tokenizer with the file path
     #tokenizer = CodeTokenizer(file_path)
-    #
     ## Tokenize the code
     #tokenizer.tokenize_code()
     #ast = syntaxTree(tokenizer.get_tokens())
